40380469fe56cbec928540cb61959e91c90cf13b
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-09-03 13:40:06 +0300

    Implement grayscale filter

--- helpers.c
+++ helpers.c
@@ -1,8 +1,21 @@
+#include <math.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE gray = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+
+            image[i][j].rgbtBlue = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtRed = gray;
+        }
+    }
+
     return;
 }
 
